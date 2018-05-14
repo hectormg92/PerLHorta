05c1e47adba561455a948b4ded1b6e6de1241848
Héctor Mulet García <hector.mulet.garcia@gmail.com>
2018-05-14 19:36:48 +0200

    capes noves

--- js/layers.js
+++ js/layers.js
@@ -1,15 +1,30 @@
-var wmsOptions = {
-	layers: "perlhorta:plh_variables_poligons",
-	format: "image/png",
-	opacity: 0.5,
-	transparent: true,
-	weight: 5,
+var layers = {
+	Parceles: L.tileLayer.wms('http://localhost:8080/geoserver/wms', {
+		layers: "perlhorta:parcel_les",
+		format: "image/png",
+		opacity: 0.5,
+		transparent: true,
+		weight: 5,
     	color: '#9c0000',
-    	fillOpacity: 0.8,
-};
-
-var wmsLayer = L.tileLayer.wms('http://localhost:8080/geoserver/wms', wmsOptions);
-
-
-
-wmsLayer.addTo(smap);
+		fillOpacity: 0.8
+	}),
+	var_pol: L.tileLayer.wms('http://localhost:8080/geoserver/wms',{
+		layers: "perlhorta:plh_variables_poligons",
+		format: "image/png",
+		opacity: 0.5,
+		transparent: true,
+		weight: 5,
+    	color: '#9c0000',
+		fillOpacity: 0.8
+	}),
+	var_lin: L.tileLayer.wms('http://localhost:8080/geoserver/wms',{
+		layers: "perlhorta:plh_variables_linies",
+		format: "image/png",	
+	}),
+	var_pnt: L.tileLayer.wms('http://localhost:8080/geoserver/wms',{
+		layers: "perlhorta:plh_variables_punts",
+		format: "image/png",		
+	}),
+	
+}
+L.control.layers(null,layers).addTo(smap);
